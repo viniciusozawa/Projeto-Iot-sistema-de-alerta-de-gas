b34a46e53a8343e51e1cc375d2c31f4beebfbed1
Japola <yudiozawa123@gmail.com>
2026-06-15 09:44:40 -0300

    feat: init project

---
+++ src/test/java/com/japola/gas/alert/AlertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.japola.gas.alert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
